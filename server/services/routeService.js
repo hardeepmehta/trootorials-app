5494fe9ad2b1bf721e8882a12be947da4f526611
aashi94 <aashigoyal94@gmail.com>
2017-07-18 01:56:45 +0000

    Add files via upload

--- server/services/routeService.js
+++ server/services/routeService.js
@@ -4,11 +4,7 @@ const apiService = require('services/apiService'),
 
 module.exports = function(app,passport) {
     require('controllers/authController')(app, passport);
-<<<<<<< HEAD
     require('controllers/courseController')(app, passport);
     require('controllers/userController')(app, passport);
-    // require('controllers/videoController')(app, passport);
-=======
     require('controllers/videoController')(app, passport);
->>>>>>> c1093d5a21f210e7202815a51049763cfbdf76c6
 };
